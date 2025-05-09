99ff3f2fccd6aa7720f0a597e2d5d080032f5ddf
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-05-09 10:15:17 +0530

    first commit

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Tabs } from "expo-router";
+import { Feather } from '@expo/vector-icons';
+export default function RootLayout() {
+    return <Tabs screenOptions={{ tabBarActiveTintColor: '#fff',tabBarShowLabel:false }}>
+        <Tabs.Screen name="index" options={{ title: 'Home', tabBarIcon: ({ size, color }) => <Feather name="home" size={size} color={color} /> }} />
+        <Tabs.Screen name="search" options={{ title: 'Search', tabBarIcon: ({ size, color }) => <Feather name="search" size={size} color={color} /> }} />
+        <Tabs.Screen name="notification" options={{ title: 'Notifications', tabBarIcon: ({ size, color }) => <Feather name="heart" size={size} color={color} /> }} />
+        <Tabs.Screen name="profile" options={{ title: 'Profile', tabBarIcon: ({ size, color }) => <Feather name="user" size={size} color={color} /> }} />
+    </Tabs>;
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,18 @@
+import { Slot } from "expo-router";
+import { ThemeProvider, DarkTheme } from '@react-navigation/native';
+export default function RootLayout() {
+  const myTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      primary: "white",
+      card: '#101010',
+
+    }
+  }
+  return (
+    <ThemeProvider value={myTheme}>
+      <Slot />
+    </ThemeProvider>
+  );
+}
