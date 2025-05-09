34837480242524af6a1736f8456f7afa8dc88947
Tarun Sai Srinivas <tarunsaisrinivas4@gmail.com>
2025-05-09 16:41:28 +0530

    path aliases

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { View, Text, StyleSheet } from "react-native";
 import React from "react";
-
+import Button from "../../components/Button";
 export default function index() {
   return (
     <View style={styles.container}>
       <Text>index</Text>
+      <Button />
     </View>
   );
 }
---
+++ components/Button.tsx
@@ -0,0 +1,9 @@
+import { Pressable, Text } from "react-native";
+
+export default function Button () {
+    return (
+        <Pressable>
+            <Text>Button</Text>
+        </Pressable>
+    );
+}
